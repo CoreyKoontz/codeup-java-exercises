03a036e0f1adad15bee3b07727adf09230272da6
Corey Koontz <corey.michael.koontz@gmail.com>
2021-03-02 23:29:39 -0600

    hello world exercise final

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,24 +14,27 @@ public class HelloWorld {
          //  by another program who in turn communicates the instructions to the target machine.
          // -Static performs type checking at compile time while dynamic does it at run time
 // TODO:    What does a compiler do?
-//
+         // Compiles the source code (.java) indo bytecode (.class)
 // TODO:    What is a virtual machine?
-//
+         // Changes your bytecode into machine code and instructs the machine at a low level
 // TODO:    What is the difference between bytecode and source code files?
-//
+         // Source code is what the developer writes (human readable)
+         // Bytecode is written in hexadecimal code that is not human readable
 // TODO:    Do you need the JDK or the JRE to play Minecraft?
-//
+         // JRE
 // TODO:    Why would you use an IDE?
-//
+        // For auto-compiling and for syntax correction
 // TODO:    What is the purpose of the out directory?
-//
+        // The out directory stores all your compiled class files
 // TODO:    Why does Java have multiple number data types? Why use one over the other?
-//
+         // I think its for performance reasons and file size control
 // TODO:    Is a string a primitive type in Java?
-//
+        // No
 // TODO:    What is the difference between explicit and implicit casting? What are specific examples?
-//
+         // Explicit is when you are going from lower precision to higher precision and it will not
+         // work unless you specify it in your code.
+         // Implicit is going from Higher precision to lower precision and it will happen implicitly
 // TODO:    What are some differences between char and String types?
-//
+         // 'char' "String"
 // TODO:    What happens if you store a number in a variable greater than the data type’s constraints?
-//
+         // it will not work unless you use explicit casting
