ff87b58526f5a856f40d9019d989554d5d0b3917
Corey Koontz <corey.michael.koontz@gmail.com>
2021-03-02 23:19:06 -0600

    adding first java exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+    }
+
+}
+
+// TODO:    What is the difference between compiled and interpreted
+//          languages? Static and dynamic? Which is Java, which is JavaScript?
+         // -Compiled code is expressed in the instructions of the target machine (machine language).
+         //  Interpreted is not directly executed by the target machine, but instead red and executed
+         //  by another program who in turn communicates the instructions to the target machine.
+         // -Static performs type checking at compile time while dynamic does it at run time
+// TODO:    What does a compiler do?
+//
+// TODO:    What is a virtual machine?
+//
+// TODO:    What is the difference between bytecode and source code files?
+//
+// TODO:    Do you need the JDK or the JRE to play Minecraft?
+//
+// TODO:    Why would you use an IDE?
+//
+// TODO:    What is the purpose of the out directory?
+//
+// TODO:    Why does Java have multiple number data types? Why use one over the other?
+//
+// TODO:    Is a string a primitive type in Java?
+//
+// TODO:    What is the difference between explicit and implicit casting? What are specific examples?
+//
+// TODO:    What are some differences between char and String types?
+//
+// TODO:    What happens if you store a number in a variable greater than the data type’s constraints?
+//
