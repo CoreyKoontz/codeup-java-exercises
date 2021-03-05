10cc580ef95dc452844a588a36d758b991c297c1
Corey Koontz <corey.michael.koontz@gmail.com>
2021-03-05 16:55:02 -0600

    Forgot to switch to new branch. Adding what I have before switching

---
+++ src/MethodsExercises.java
@@ -0,0 +1,142 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MethodsExercises {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println(tryGetSalutation("Corey"));
+//
+//    }
+
+//----------- Method Notes -------------------------------------
+
+    // Access Modifiers
+
+//    public static String tryGetSalutation(String name) {
+//
+//        if((name == null) || (name.equals(""))) { // ** This is a Check
+//        return "No name provided";
+//        } else {
+//            return getSalutation(name, "Hello, ");
+//        }
+//    }
+//
+//    public static String getSalutation(String name, String greeting) {
+//        return greeting + name.toUpperCase();
+//    }
+//
+//
+//    // Static vs Instance
+//
+//    public static int doTheMathStatic(){
+//        return 1 + 2;
+//    }
+//
+//    public int doTheMathInstance() {
+//        return 1 + 2;
+//    }
+//
+//
+//    // *** Returning vs Void ***
+//
+//    public static String getGreeting () {
+//        return "Hi";
+//    }
+//
+//    public static void printGreeting() {
+//        System.out.println();
+//    }
+//
+
+    /* TODO:
+     *   -Write a public static method which returns an integer
+     *   -The method should take in *two parameters* and *return* the product of multiplying them
+     *   -Print the output to the console by calling the method, passing in params, and logging its output
+     *   BONUS: Use the Scanner class to get the numbers from the user input
+     * */
+
+
+
+
+//    public static void main(String[] args) {
+////        getX();
+////        getY();
+////        productOf(x, y);
+//        printProduct();
+//
+//
+//    }
+//
+//
+//    public static int getX() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("What is the value of x?");
+//        int x = sc.nextInt();
+//        return x;
+//    }
+//    public static int getY() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Whats the value of y?");
+//        int y = sc.nextInt();
+//        return y;
+//    }
+//    public static int productOf(int x, int y) {
+//        return x * y;
+//    }
+//    public static void printProduct() {
+//        System.out.println(productOf(getX(), getY()));
+//    }
+
+
+    // *** Method Overloading ***
+
+//    public static int doMath() {
+//        return 1 + 1;
+//    }
+//
+//    // !!!!!!!! ALL we are changing is parameters !!!!!!!!
+//
+//    public static int doMath(int num1) {
+//        return num1 + (1 + 1);
+//    }
+//
+//    public static int doMath(int num1, int num2) {
+//        return num1 + num2;
+//    }
+
+    // ---- Overloading Exercise ----------------------------
+
+    /* TODO: Write a set of overloaded public static void methods which can take in 1-3 names and print them to the console
+    *   -Call each of the methods, passing in the names for each
+    *   -Minus 1000pts if you don't use an oxford comma on the 3 param version
+            *   BONUS: Use the Scanner class to get the names from user input
+            * Bonus: create a logic method that
+    *  */
+//    Scanner sc = new Scanner(System.in);
+//
+//    public static void main(String[] args) {
+//
+//        takeNames("jim", "mike", "pam");
+//
+//    }
+//
+//
+//        public static void takeNames (String name1){
+//            System.out.printf("Hello, %s.", name1);
+//        }
+//        public static void takeNames (String name1, String name2){
+//            System.out.printf("Hello, %s and %s.", name1, name2);
+//
+//        }
+//        public static void takeNames (String name1, String name2, String name3){
+//            System.out.printf("Hello, %s, %s, and %s.", name1, name2, name3);
+//
+//        }
+
+    /*
+
+     *  */
+
+
+}
